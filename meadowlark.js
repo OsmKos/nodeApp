ff165dd2fadfa2991a95c5e6a45518765f08a373
OsmKos <osmkos10@gmail.com>
2017-02-23 01:33:23 +0300

    MongoDB

--- meadowlark.js
+++ meadowlark.js
@@ -1,13 +1,16 @@
-var express = require('express'),
+var http = require('http'),
+	express = require('express'),
 	fortune = require('./lib/fortune.js'),
-	formidable = require('formidable');
-
-var credentials = require('./credentials.js');
-var emailService = require('./lib/email.js')(credentials);
+	formidable = require('formidable'),
+	fs = require('fs'),
+	Vacation = require('./models/vacation.js'),
+	VacationInSeasonListener = require('./models/vacationInSeasonListener.js');
 
 var app = express();
 
+var credentials = require('./credentials.js');
 
+var emailService = require('./lib/email.js')(credentials);
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars').create({
@@ -25,14 +28,145 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+// use domains for better error handling
+app.use(function(req, res, next){
+    // create a domain for this request
+    var domain = require('domain').create();
+    // handle errors on this domain
+    domain.on('error', function(err){
+        console.error('DOMAIN ERROR CAUGHT\n', err.stack);
+        try {
+            // failsafe shutdown in 5 seconds
+            setTimeout(function(){
+                console.error('Failsafe shutdown.');
+                process.exit(1);
+            }, 5000);
+
+            // disconnect from the cluster
+            var worker = require('cluster').worker;
+            if(worker) worker.disconnect();
+
+            // stop taking new requests
+            server.close();
+
+            try {
+                // attempt to use Express error route
+                next(err);
+            } catch(error){
+                // if Express error route failed, try
+                // plain Node response
+                console.error('Express error mechanism failed.\n', error.stack);
+                res.statusCode = 500;
+                res.setHeader('content-type', 'text/plain');
+                res.end('Server error.');
+            }
+        } catch(error){
+            console.error('Unable to send 500 response.\n', error.stack);
+        }
+    });
+
+    // add the request and response objects to the domain
+    domain.add(req);
+    domain.add(res);
+
+    // execute the rest of the request chain in the domain
+    domain.run(next);
+});
+
+// logging
+switch(app.get('env')){
+    case 'development':
+    	// compact, colorful dev logging
+    	app.use(require('morgan')('dev'));
+        break;
+    case 'production':
+        // module 'express-logger' supports daily log rotation
+        app.use(require('express-logger')({ path: "D:/js/projects/nodeApp" + '/log/requests.log'}));
+        break;
+}
+
+var MongoSessionStore = require('session-mongoose')(require('connect'));
+var sessionStore = new MongoSessionStore({ url: credentials.mongo[app.get('env')].connectionString });
+
 app.use(require('cookie-parser')(credentials.cookieSecret));
 app.use(require('express-session')({
     resave: false,
     saveUninitialized: false,
     secret: credentials.cookieSecret,
+	store: sessionStore,
 }));
-app.use(express.static("D:/js/projects/nodeApp" + "/public"));
+app.use(express.static("D:/js/projects/nodeApp" + '/public'));
 app.use(require('body-parser')());
+//межсайтовый доступ к api
+app.use('/api', require('cors')());
+
+// database configuration
+var mongoose = require('mongoose');
+var options = {
+    server: {
+       socketOptions: { keepAlive: 1 }
+    }
+};
+switch(app.get('env')){
+    case 'development':
+        mongoose.connect(credentials.mongo.development.connectionString, options);
+        break;
+    case 'production':
+        mongoose.connect(credentials.mongo.production.connectionString, options);
+        break;
+    default:
+        throw new Error('Unknown execution environment: ' + app.get('env'));
+}
+
+// initialize vacations
+Vacation.find(function(err, vacations){
+    if(vacations.length) return;
+
+    new Vacation({
+        name: 'Hood River Day Trip',
+        slug: 'hood-river-day-trip',
+        category: 'Day Trip',
+        sku: 'HR199',
+        description: 'Spend a day sailing on the Columbia and ' +
+            'enjoying craft beers in Hood River!',
+        priceInCents: 9995,
+        tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
+        inSeason: true,
+        maximumGuests: 16,
+        available: true,
+        packagesSold: 0,
+    }).save();
+
+    new Vacation({
+        name: 'Oregon Coast Getaway',
+        slug: 'oregon-coast-getaway',
+        category: 'Weekend Getaway',
+        sku: 'OC39',
+        description: 'Enjoy the ocean air and quaint coastal towns!',
+        priceInCents: 269995,
+        tags: ['weekend getaway', 'oregon coast', 'beachcombing'],
+        inSeason: false,
+        maximumGuests: 8,
+        available: true,
+        packagesSold: 0,
+    }).save();
+
+    new Vacation({
+        name: 'Rock Climbing in Bend',
+        slug: 'rock-climbing-in-bend',
+        category: 'Adventure',
+        sku: 'B99',
+        description: 'Experience the thrill of rock climbing in the high desert.',
+        priceInCents: 289995,
+        tags: ['weekend getaway', 'bend', 'high desert', 'rock climbing', 'hiking', 'skiing'],
+        inSeason: true,
+        requiresWaiver: true,
+        maximumGuests: 4,
+        available: false,
+        packagesSold: 0,
+        notes: 'The tour guide is currently recovering from a skiing accident.',
+    }).save();
+});
 
 // flash message middleware
 app.use(function(req, res, next){
@@ -86,246 +220,39 @@ app.use(function(req, res, next){
  	next();
 });
 
-app.get('/', function(req, res) {
-	res.render('home');
+
+// create "admin" subdomain...this should appear
+// before all your other routes
+var admin = express.Router();
+app.use(require('vhost')('admin.*', admin));
+
+// create admin routes; these can be defined anywhere
+admin.get('/', function(req, res){
+	res.render('admin/home');
 });
-app.get('/about', function(req,res){
-	res.render('about', {
-		fortune: fortune.getFortune(),
-		pageTestScript: '/qa/tests-about.js'
-	} );
-});
-app.get('/tours/request-group-rate', function(req, res){
-	res.render('tours/request-group-rate');
-});
-app.get('/jquery-test', function(req, res){
-	res.render('jquery-test');
-});
-app.get('/nursery-rhyme', function(req, res){
-	res.render('nursery-rhyme');
-});
-app.get('/data/nursery-rhyme', function(req, res){
-	res.json({
-		animal: 'squirrel',
-		bodyPart: 'tail',
-		adjective: 'bushy',
-		noun: 'heck',
-	});
-});
-app.get('/thank-you', function(req, res){
-	res.render('thank-you');
-});
-app.get('/newsletter', function(req, res){
-	res.render('newsletter');
+admin.get('/users', function(req, res){
+	res.render('admin/users');
 });
 
-// for now, we're mocking NewsletterSignup:
-function NewsletterSignup(){
-}
-NewsletterSignup.prototype.save = function(cb){
-	cb();
-};
 
-// mocking product database
-function Product(){
-}
-Product.find = function(conditions, fields, options, cb){
-	if(typeof conditions==='function') {
-		cb = conditions;
-		conditions = {};
-		fields = null;
-		options = {};
-	} else if(typeof fields==='function') {
-		cb = fields;
-		fields = null;
-		options = {};
-	} else if(typeof options==='function') {
-		cb = options;
-		options = {};
-	}
-	var products = [
-		{
-			name: 'Hood River Tour',
-			slug: 'hood-river',
-			category: 'tour',
-			maximumGuests: 15,
-			sku: 723,
-		},
-		{
-			name: 'Oregon Coast Tour',
-			slug: 'oregon-coast',
-			category: 'tour',
-			maximumGuests: 10,
-			sku: 446,
-		},
-		{
-			name: 'Rock Climbing in Bend',
-			slug: 'rock-climbing/bend',
-			category: 'adventure',
-			requiresWaiver: true,
-			maximumGuests: 4,
-			sku: 944,
-		}
-	];
-	cb(null, products.filter(function(p) {
-		if(conditions.category && p.category!==conditions.category) return false;
-		if(conditions.slug && p.slug!==conditions.slug) return false;
-		if(isFinite(conditions.sku) && p.sku!==Number(conditions.sku)) return false;
-		return true;
-	}));
-};
-Product.findOne = function(conditions, fields, options, cb){
-	if(typeof conditions==='function') {
-		cb = conditions;
-		conditions = {};
-		fields = null;
-		options = {};
-	} else if(typeof fields==='function') {
-		cb = fields;
-		fields = null;
-		options = {};
-	} else if(typeof options==='function') {
-		cb = options;
-		options = {};
-	}
-	Product.find(conditions, fields, options, function(err, products){
-		cb(err, products && products.length ? products[0] : null);
-	});
-};
+// add routes
+require('./routes.js')(app);
 
-var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+// add support for auto views
+var autoViews = {};
 
-app.post('/newsletter', function(req, res){
-	var name = req.body.name || '', email = req.body.email || '';
-	// input validation
-	if(!email.match(VALID_EMAIL_REGEX)) {
-		if(req.xhr) return res.json({ error: 'Invalid name email address.' });
-		req.session.flash = {
-			type: 'danger',
-			intro: 'Validation error!',
-			message: 'The email address you entered was  not valid.',
-		};
-		return res.redirect(303, '/newsletter/archive');
-	}
-	new NewsletterSignup({ name: name, email: email }).save(function(err){
-		if(err) {
-			if(req.xhr) return res.json({ error: 'Database error.' });
-			req.session.flash = {
-				type: 'danger',
-				intro: 'Database error!',
-				message: 'There was a database error; please try again later.',
-			};
-			return res.redirect(303, '/newsletter/archive');
-		}
-		if(req.xhr) return res.json({ success: true });
-		req.session.flash = {
-			type: 'success',
-			intro: 'Thank you!',
-			message: 'You have now been signed up for the newsletter.',
-		};
-		return res.redirect(303, '/newsletter/archive');
-	});
-});
-app.get('/newsletter/archive', function(req, res){
-	res.render('newsletter/archive');
-});
-app.get('/contest/vacation-photo', function(req, res){
-    var now = new Date();
-    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
-});
-app.post('/contest/vacation-photo/:year/:month', function(req, res){
-    var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files){
-        if(err) return res.redirect(303, '/error');
-        console.log('received fields:');
-        console.log(fields);
-        console.log('received files:');
-        console.log(files);
-        res.redirect(303, '/thank-you');
-    });
-});
-app.get('/tours/:tour', function(req, res, next){
-	Product.findOne({ category: 'tour', slug: req.params.tour }, function(err, tour){
-		if(err) return next(err);
-		if(!tour) return next();
-		res.render('tour', { tour: tour });
-	});
-});
-app.get('/adventures/:subcat/:name', function(req, res, next){
-	Product.findOne({ category: 'adventure', slug: req.params.subcat + '/' + req.params.name  }, function(err, adventure){
-		if(err) return next(err);
-		if(!adventure) return next();
-		res.render('adventure', { adventure: adventure });
-	});
-});
-
-var cartValidation = require('./lib/cartValidation.js');
-
-app.use(cartValidation.checkWaivers);
-app.use(cartValidation.checkGuestCounts);
-
-app.post('/cart/add', function(req, res, next){
-	var cart = req.session.cart || (req.session.cart = []);
-	Product.findOne({ sku: req.body.sku }, function(err, product){
-		if(err) return next(err);
-		if(!product) return next(new Error('Unknown product SKU: ' + req.body.sku));
-		cart.push({
-			product: product,
-			guests: req.body.guests || 0,
-		});
-		res.redirect(303, '/cart');
-	});
-});
-
-app.get('/cart', function(req, res, next){
-	var cart = req.session.cart;
-	if(!cart) next();
-  else
-	res.render('cart', { cart: cart });
-});
-
-app.get('/cart/checkout', function(req, res, next) {
-    var cart = req.session.cart;
-    if (!cart) next();
-    else
-    res.render('cart-checkout');
-});
-app.get('/cart/thank-you', function(req, res) {
-    res.render('cart-thank-you', {
-        cart: req.session.cart
-    });
-});
-app.get('/email/cart/thank-you', function(req, res) {
-    res.render('email/cart-thank-you', {
-        cart: req.session.cart,
-        layout: null
-    });
-});
-app.post('/cart/checkout', function(req, res) {
-    var cart = req.session.cart;
-    if (!cart) next(new Error('Cart does not exist.'));
-    var name = req.body.name || '',
-        email = req.body.email || '';
-    // input validation
-    if (!email.match(VALID_EMAIL_REGEX)) return res.next(new Error('Invalid email address.'));
-    // assign a random cart ID; normally we would use a database ID here
-    cart.number = Math.random().toString().replace(/^0\.0*/, '');
-    cart.billing = {
-        name: name,
-        email: email,
-    };
-    res.render('email/cart-thank-you', {
-        layout: null,
-        cart: cart
-    }, function(err, html) {
-        if (err) console.log('error in email template');
-        emailService.send(cart.billing.email,
-            'Thank you for booking your trip with Meadowlark Travel!',
-            html);
-    });
-    res.render('cart-thank-you', {
-        cart: cart
-    });
+app.use(function(req,res,next){
+    var path = req.path.toLowerCase();
+    // check cache; if it's there, render the view
+    if(autoViews[path]) return res.render(autoViews[path]);
+    // if it's not in the cache, see if there's
+    // a .handlebars file that matches
+    if(fs.existsSync("D:/js/projects/nodeApp" + '/views' + path + '.handlebars')){
+        autoViews[path] = path.replace(/^\//, '');
+        return res.render(autoViews[path]);
+    }
+    // no view found; pass on to 404 handler
+    next();
 });
 
 // 404 catch-all handler (middleware)
@@ -341,7 +268,20 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.' );
-});
+var server;
+
+function startServer() {
+    server = http.createServer(app).listen(app.get('port'), function(){
+      console.log( 'Express started in ' + app.get('env') +
+        ' mode on http://localhost:' + app.get('port') +
+        '; press Ctrl-C to terminate.' );
+    });
+}
+
+if(require.main === module){
+    // application run directly; start app server
+    startServer();
+} else {
+    // application imported as a module via "require": export function to create server
+    module.exports = startServer;
+}
